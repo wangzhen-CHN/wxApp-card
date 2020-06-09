73a38a60a2d085a6eb017ef74bb430ad30698d4a
wangzhen-CHN <137014311@qq.com>
2020-06-09 20:48:47 +0800

    init

---
+++ miniprogram/app.js
@@ -0,0 +1,45 @@
+//app.js
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        // env 参数说明：
+        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        //   如不填则使用默认环境（第一个创建的环境）
+        env: 'credit-wp9pu',
+        traceUser: true,
+      })
+    }
+    this.globalData = {}
+      //  // 获取用户信息
+      //  wx.getSetting({
+      //   success: res => {
+      //     if (res.authSetting['scope.userInfo']) {
+      //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+      //       wx.getUserInfo({
+      //         success: res => {
+      //           console.log(res)
+      //           // app.globalData.openid = res.openid
+      //         }
+      //       })
+      //     }
+      //   }
+      // })
+       // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result)
+        this.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+  }
+})
