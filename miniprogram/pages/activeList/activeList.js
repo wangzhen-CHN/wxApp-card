de5930f801105249aef06cde7137bb12c2fd2372
wangzhen-CHN <137014311@qq.com>
2020-08-17 20:00:15 +0800

    项目初步完成

--- miniprogram/pages/activeList/activeList.js
+++ miniprogram/pages/activeList/activeList.js
@@ -1,13 +1,33 @@
-//cardlist.js
+//activityList.js
 const app = getApp()
 
 Page({
   data: {
     openid: '',
-    columns: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
-    minHeight:{ minHeight: 50 },
-    popupShow: false,
-  
+    bankName: '选择活动方',
+    bankCode:'',
+    name: '',
+    content: '',
+    time: '',
+    remake: '',
+    activityList:[],
+    columns: [
+      { text: '中国银行', value: 'BOC' },
+      { text: '工商银行', value: 'ICBC' },
+      { text: '建设银行', value: 'CCB' },
+      { text: '浦发银行', value: 'SPDB' },
+      { text: '招商银行', value: 'CMB' },
+      { text: '广发银行', value: 'GF' },
+      { text: '微信', value: 'WX' },
+      { text: '支付宝', value: 'ZFB' },
+      { text: '其他', value: 'OTHER' },
+      // { text: '农业银行', disabled: true },
+    ],
+    isShowPopup: false,
+    isShowBankPopup: false,
+    showPopupDetail: false,
+    isEdit: false,
+
   },
   onLoad: function () {
     if (app.globalData.openid) {
@@ -18,141 +38,66 @@ Page({
     this.setData({
       today: new Date().getUTCDate().toString()
     })
-    console.log(this.data.today)
-    this.onQuery()
+    this.doQuery()
   },
   handleAdd() {
     this.setData({
-      popupShow: true
+      isShowPopup: true
     })
   },
-  handleDetail(e) {
+  onBankPopup() {
     this.setData({
-      popupShow: true
+     isShowBankPopup: true
     })
-    console.log(e.currentTarget.dataset)
   },
+  onBankPickerConfirm(event) {
+    const { picker, value, index } = event.detail;
+    console.log(event.detail)
+    this.setData({
+      isShowBankPopup: false,
+      bankName:value.text,
+      bankCode:value.value,
+     })
+  },
+  // handleDetail(e) {
+  //   this.setData({
+  //     isShowPopup: true
+  //   })
+  //   console.log(e.currentTarget.dataset)
+  // },
   onPopupClose() {
     this.setData({
-      popupShow: false
+      isShowPopup: false
     })
   },
-  setFormValue(event) {
-    if (event.currentTarget.dataset.params==='billDay') {
-      this.setData({
-        'billday': event.detail.value
-      })
-    }
-    const obj = {}
-    obj[event.currentTarget.dataset.params] = event.detail.value
+  onBankPopupClose() {
     this.setData({
-      'formData': {
-        ...this.data.formData,
-        ...obj
-      }
+      isShowBankPopup: false
     })
   },
-
-  getBankInfo(event) {
-    this.setData({
-      'cardNo': event.detail
-    })
-    if (event.detail.length > 5 && this.data.sendCloud) {
-      // 调用云函数
-      wx.cloud.callFunction({
-        name: 'bankinfo',
-        data: {
-          cardNo: event.detail
-        },
-        success: res => {
-          console.log('[云函数] [bankinfo] : ', res.result)
-          if (res.result.bankCode != 400) {
-            this.setData({
-              'bankIcon': `../../images/icon-bank/${res.result.bankCode}.png`,
-              'bankInfo': res.result,
-              'cardType': res.result.cardType
-            })
-          }
-        },
-        fail: err => {
-          console.error('[云函数] [bankinfo] 调用失败', err)
-        }
-      })
-      this.setData({
-        'sendCloud': false
-      })
-    }
-    if (event.detail.length > 0 && this.data.checkOrg) {
-      const orgNo = event.detail.slice(0, 1)
-      switch (orgNo) {
-        case '4':
-          this.setData({
-            'cardOrg': 'visa',
-            'orgIcon': '../../images/icon-bank/card-org/card-visa.png'
-          })
-          break;
-        case '5':
-          this.setData({
-            'cardOrg': 'master',
-            'orgIcon': '../../images/icon-bank/card-org/card-master.png'
-          })
-          break;
-        case '6':
-          this.setData({
-            'cardOrg': 'unionpay',
-            'orgIcon': '../../images/icon-bank/card-org/card-unionpay.png'
-          })
-          break;
-        default:
-          this.setData({
-            'orgIcon': ''
-          })
-          break;
-      }
-      this.setData({
-        'checkOrg': false
-      })
-    }
-    if (event.detail.length === 0) {
-      this.setData({
-        'sendCloud': true,
-        'checkOrg': true,
-        'billday':'*',
-        'orgIcon': '',
-        'bankIcon':'',
-        'cardNo': '**** **** **** ****',
-      })
-    }
-  },
-  clearBankInfo(){
-    this.setData({
-      'sendCloud': true,
-      'checkOrg': true,
-      'orgIcon': '',
-      'billday':'*',
-      'bankIcon':'',
-      'cardNo': '**** **** **** ****',
-    })
-  },
+  
   formSubmit() {
     const {
-      cardOrg,
-      cardNo,
-      bankInfo,
-      formData,
+      bankName,
+      bankCode,
+      name,
+      content,
+      time,
+      remake,
     } = this.data
-    const cardInfo = {
-      cardOrg,
-      cardNo,
-      ...formData,
-      ...bankInfo
-    }
-    console.log(cardInfo)
-    this.onAdd(cardInfo) //添加到数据库
+
+    this.doAdd({
+      bankName,
+      bankCode,
+      name,
+      content,
+      time,
+      remake,
+    }) //添加到数据库
   },
-  onAdd: function (params) {
+  doAdd: function (params) {
     const db = wx.cloud.database()
-    db.collection('card_list').add({
+    db.collection('activity_list').add({
       data: {
         ...params
       },
@@ -166,9 +111,9 @@ Page({
           title: '新增记录成功',
         })
         this.setData({
-          popupShow: false
+          isShowPopup: false
         })
-        this.onQuery()
+        this.doQuery()
         console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
       },
       fail: err => {
@@ -181,20 +126,15 @@ Page({
     })
   },
 
-  onQuery: function () {
+  doQuery: function () {
     const db = wx.cloud.database()
     // 查询当前用户所有的 card_list
-    db.collection('card_list').where({
+    db.collection('activity_list').where({
       _openid: this.data.openid
     }).get({
       success: res => {
-        res.data.map(item=>{
-          // item.cardNoFormat=item.cardNo.replace(/\s/g,'').replace(/[^\d]/g,'').replace(/(\d{4})(?=\d)/g,'$1 ')
-          item.cardNoFormat = item.cardNo.replace(/^(\d{4})\d+(\d{4})$/, "$1 **** **** $2");
-        })
         this.setData({
-          cardList: res.data
-          // cardList: []
+          activityList: res.data
         })
         console.log('[数据库] [查询记录] 成功: ', res)
       },
@@ -207,20 +147,5 @@ Page({
       }
     })
   },
-  copyCardNo(e) {
-    wx.setClipboardData({
-      data: e.currentTarget.dataset.cardno,
-      success: function (res) {
-        wx.getClipboardData({
-          success: function (res) {
-            wx.showToast({
-              icon:'none',
-              duration:1000,
-              title: '复制成功'
-            })
-          }
-        })
-      }
-    })
-  }
+
 })
